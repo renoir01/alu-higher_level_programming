50753a57ae204421e8f70a5cd14f8b9718bc0461
renoir01 <re.kaze@alustudent.com>
2023-08-04 11:18:19 +0000

    my project in python

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-'''This script fetches the status information from the URL
- 'https://alu-intranet.hbtn.io/status' and prints the results in a formatted way.
+# 0-hbtn_status.py
+"""This module or script
+fetches https://alu-intranet.hbtn.io/status
+using the urllib library
+"""
 
-Dependencies:
-- requests library (install using 'pip install requests')
 
-Usage:
-python fetch_status.py
+from urllib import request, error
 
-Example Output:
-'''
-import urllib.request
-
-if __name__ == "__main__":
-    '''Something here
-    '''
-    url = 'https://alu-intranet.hbtn.io/status'
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        data = response.read()
-        info = f"Body response:\n\t- type: {type(data)}\n\t- \
-content: {data}\n\t- utf8 content: {data.decode('utf-8')}"
-        print(info)
+try:
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        response_body = response.read()
+        response_text = response_body.decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(response_body))
+        print("\t- content:", response_body)
+        print("\t- utf8 content:", response_text)
+except error.URLError:
+    custom_status = bytes("Custom status", encoding="utf-8")
+    custom_status_text = custom_status.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(custom_status))
+    print("\t- content:", custom_status)
+    print("\t- utf8 content:", custom_status_text)
