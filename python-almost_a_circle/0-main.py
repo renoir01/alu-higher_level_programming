d1985d5f946083aaab49509afcfe888a0f37dc0c
renoir01 <r.kaze@alustudent.com>
2023-08-16 00:45:20 -0700

    project on circle

--- python-almost_a_circle/0-main.py
+++ python-almost_a_circle/0-main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ 0-main """
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 if __name__ == "__main__":
-
+    # Test cases for Base class
     b1 = Base()
     print(b1.id)
 
@@ -18,3 +20,15 @@ if __name__ == "__main__":
 
     b5 = Base()
     print(b5.id)
+
+    # Test cases for Rectangle class
+    r1 = Rectangle(2, 3)
+    print(r1.id)
+    r2 = Rectangle(4, 5, 1, 2, 6)
+    print(r2.id)
+
+    # Test cases for Square class
+    s1 = Square(5)
+    print(s1.id)
+    s2 = Square(7, 2, 3, 8)
+    print(s2.id)
